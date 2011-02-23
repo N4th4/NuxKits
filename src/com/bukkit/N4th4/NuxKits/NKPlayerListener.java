4f6f314c8a5a875169af4e104d7989555ef128cc
Nathanaël Restori <nathidem@hotmail.com>
2011-02-23 19:03:43 +0100

    Use "static" config path

--- src/com/bukkit/N4th4/NuxKits/NKPlayerListener.java
+++ src/com/bukkit/N4th4/NuxKits/NKPlayerListener.java
@@ -89,12 +89,12 @@ public class NKPlayerListener extends PlayerListener {
     }
 
     private void loadConfig() {
-        File configFile = new File(plugin.getDataFolder() + "/config.yml");
+        File configFile = new File("plugins/NuxNoob/config.yml");
         if (configFile.exists()) {
             config = new Configuration(configFile);
             config.load();
         } else {
-            NKLogger.severe("Fichier de configuration non trouvé : " + plugin.getDataFolder() + "/config.yml");
+            NKLogger.severe("Fichier de configuration non trouvé : plugins/NuxNoob/config.yml");
         }
     }
 
