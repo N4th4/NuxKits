89d4d40badb2553aeee178daaa69970c40d32ec5
Nathanaël Restori <nathidem@hotmail.com>
2011-04-10 12:33:45 +0200

    Move to Permissions

--- src/com/bukkit/N4th4/NuxKits/NKPermissions.java
+++ src/com/bukkit/N4th4/NuxKits/NKPermissions.java
@@ -1,36 +1,36 @@
 package com.bukkit.N4th4.NuxKits;
 
-import org.anjocaido.groupmanager.GroupManager;
-import org.anjocaido.groupmanager.dataholder.worlds.WorldsHolder;
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+
+import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 public class NKPermissions {
-    private static WorldsHolder wd;
+    private static PermissionHandler Permissions;
+    private static Server server;
 
     public static void initialize(NuxKits plugin) {
-        Plugin p = plugin.getServer().getPluginManager().getPlugin("GroupManager");
-        if (p != null) {
-            if (!plugin.getServer().getPluginManager().isPluginEnabled(p)) {
-                plugin.getServer().getPluginManager().enablePlugin(p);
-            }
-            GroupManager gm = (GroupManager) p;
-            wd = gm.getWorldsHolder();
+        server = plugin.getServer();
+        Plugin test = server.getPluginManager().getPlugin("Permissions");
+
+        if (test != null) {
+            Permissions = ((Permissions) test).getHandler();
         } else {
-            NKLogger.severe("Plugin GroupManager not found");
-            plugin.getPluginLoader().disablePlugin(plugin);
+            NKLogger.info("Permission system not detected, defaulting to OP");
         }
     }
 
     public static String getGroup(String playerName) {
-        return wd.getWorldPermissionsByPlayerName(playerName).getGroup(playerName);
+        return Permissions.getGroup(server.getPlayer(playerName).getWorld().getName(), playerName);
     }
 
     public static boolean has(CommandSender player, String string) {
         if (player instanceof Player) {
             Player playerBis = (Player) player;
-            return wd.getWorldPermissions(playerBis).has(playerBis, string);
+            return Permissions.has(playerBis, string);
         } else {
             return player.isOp();
         }
